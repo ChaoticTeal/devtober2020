e8e64c42307da809e230d88f6cc77d65a5f77333
PurpleNinjaLink <jawhittom@gmail.com>
2020-10-02 21:36:46 -0500

    Imports Art, Starts Movement
    Imports character, environment, and enemy art. Adds the InputSystem package and begins to apply it to player movement.

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.valueType.Equals(typeof(Vector2)))
+        {
+            moveInput = callbackContext.ReadValue<Vector2>();
+            rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+        }
+    }
+}
